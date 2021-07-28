8b16b2add281bdbc3855ca7c7607f7284450d49a
memo figueredo <memof80@gmail.com>
2021-07-28 17:52:56 -0500

    add files images and new viewtableviewcell

---
+++ FoodPin/RestaurantTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RestaurantTableViewCell.swift
+//  FoodPin
+//
+//  Created by Memo Figueredo on 28/7/21.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- FoodPin/RestaurantTableViewController.swift
+++ FoodPin/RestaurantTableViewController.swift
@@ -17,6 +17,8 @@ class RestaurantTableViewController: UITableViewController {
 //    5. Llame a la función apply () de la fuente de datos para completar los datos.
     
     var restaurantNames = ["Cafe Deadend","Homei", "Teakha", "Cafe Loisl","Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery","Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif","Graham Avenue Meats", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional","Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+    
+    var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster", "forkee", "posatelier", "bourkestreetbakery", "haigh", "palomino", "upstate", "traif", "graham", "waffleandwolf", "fiveleaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "cask"]
 //   tree: usamos lazy porque su valor inicial no se puede recuperar hasta que finalice la iniacializacion de la instancia
    lazy var dataSource = configureDataSource()
 
@@ -53,7 +55,7 @@ class RestaurantTableViewController: UITableViewController {
                 let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
                     
                 cell.textLabel?.text = restaurantName
-                cell.imageView?.image = UIImage(named: "restaurant")
+                cell.imageView?.image = UIImage(named: self.restaurantImages[indexPath.row])
                 
                 return cell
                 
